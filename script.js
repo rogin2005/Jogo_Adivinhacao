98a4ffdc9efa4e0625c8feb6e88197dcaf68c36c
Roger Ferreira Saraiva <rogerferreirasaraiva@gmail.com>
2025-11-29 22:23:36 -0300

    correção da lógica de acertos ou erros

--- script.js
+++ script.js
@@ -15,30 +15,40 @@ function Adivinhar() {
     let minuto = horario.getMinutes()
     let segundo = horario.getSeconds()
     let palpnum = Number(palptxt.value)
-    
+
+    if (tentativas == 0) {
+        tentativas = false
+    }
+
     if (palpnum < 1 || palpnum > 100) {
         alert('Digite um número entre 1 e 100 para iniciar!')
-    } else {
-        tentativas -= 1
-        tentxt.innerHTML = `Tentativas restantes: ${tentativas}`
+    } else if (tentativas) {
+        console.log('o numero é: ' + pc)
         if (palpnum > pc) {
             feedtxt.innerHTML = "Ops, seu palpite foi alto, tente um número mais baixo...&#x2B07;"
-        } else if (palpnum < pc) {
+            tentativas -= 1
+        } 
+        if (palpnum < pc) {
             feedtxt.innerHTML = "Ops, seu palpite foi baixo, tente um número mais alto...&#x2B06;"
-        } else {
+            tentativas -= 1
+        } 
+        if (palpnum == pc) {
             // acertou
             feedtxt.innerHTML = "Parabéns, você conseguiu acertar o número secreto! &#x1F389;&#x1F389;&#x1F389;"
             sel.innerHTML += `<option>Ganhou às ${hora}:${minuto}:${segundo}&#x1F60E;</option>`
             botaoadv.disabled = true
             botaonew.disabled = false
-        }
+
+        } 
         if (tentativas == 0) {
             // acabou as tentativas
-            feedtxt.innerHTML = "Ah que pena, dessa vez o pc venceu, que tal tentar novamente? &#x1F614;"
-            sel.innerHTML += `<option>Perdeu às ${hora}:${minuto}:${segundo}&#x1F614;</option>`
             botaoadv.disabled = true
             botaonew.disabled = false
+            feedtxt.innerHTML = "Ah que pena, dessa vez o pc venceu, que tal tentar novamente? &#x1F614;"
+            tentxt.innerHTML = `Tentativas restantes: 0`
+            sel.innerHTML += `<option>Perdeu às ${hora}:${minuto}:${segundo}&#x1F614;</option>`
         }
+        tentxt.innerHTML = `Tentativas restantes: ${tentativas}`
     }
 
     //sel.innerHTML += `<option>User clicou em ${hora}:${minuto}:${segundo}</option>`
